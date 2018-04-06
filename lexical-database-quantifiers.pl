67b98a28866640e7f1d9a8b5b00a980da0bd623e
Pedro Daniel Reis <up201506046@fe.up.pt>
2018-04-06 14:56:51 +0100

    Started lexicon and dictionary

--- lexical-database-quantifiers.pl
+++
@@ -1,6 +0,0 @@
-/* quantificador(Nome, Num, Gen). */
-quantificador (o,singular,m).
-quantificador (os,plural,m).
-quantificador (a,singular,f).
-quantificador (as,plural,f).
-
--- lexical-database-verbs.pl
+++
@@ -1,5 +0,0 @@
-/* verbo (Verbo, Num). */
-verbo(fica, singular).
-verbo(ficam, plural).
-verbo(custa, singular).
-verbo(custam, plural).
